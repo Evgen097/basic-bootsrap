64cc2e0214b9a33822fff5cbd8168ace9ffc61cc
Evgen Kaban <k0990245853@gmail.com>
2018-08-27 12:19:02 +0300

    First commit

---
+++ 1.js
@@ -0,0 +1,39 @@
+// /**
+//  * Created by Evgen on 27.08.2018.
+//  */
+// Создание git  в папкe:
+//     git init
+//
+// Проверка статуса:
+//     git status
+//
+// Нужно додать все файлы в гит репозиторий:
+//     git add .
+//
+//     А потом закомитить
+// git commit -m "first commit"
+//
+// Посмотреть лог коммитов и выбрать нужный:
+//     git log --oneline
+//
+// Восстановить предыдущий коммит, нужнов ввести номер из комманды "git log --oneline"
+// git checkout <second commit's number> index.html
+// git checkout ca67f66
+//
+// Вернутся в последний коммит:
+//     git reset HEAD
+// git checkout master
+//
+//
+//
+// Сначала создаем репозиторий на Git, копируем url нового репозитория и выполняем комманды:
+//
+//     git remote add origin <repository URL>
+// git remote add origin https://github.com/Evgen097/mean-bootsrap-test.git
+//
+//     git push -u origin master
+//
+//
+// Клонировать репозиторий с Git
+// git clone <repository URL>
+// git clone https://github.com/Evgen097/mean-bootsrap-test.git
